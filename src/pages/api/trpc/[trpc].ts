61180442272c8a91b121167add0cf7b9713b034d
Jeremy Tan <jeremytkw98@gmail.com>
2022-09-22 18:48:00 -0700

    cors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,8 +2,9 @@ import * as trpcNext from "@trpc/server/adapters/next";
 
 import { serverRouter } from "@/server/router";
 import { createContext } from "@/server/context";
+import { withCors } from "./cors";
 
-export default trpcNext.createNextApiHandler({
+export default withCors(trpcNext.createNextApiHandler({
   router: serverRouter,
   createContext,
-});
+}));
